e5dc5df647049d4cf0467f7c8e5b98cb6e48c894
cs-schmidt <ebs.euler@gmail.com>
2023-07-15 22:29:41 -0600

    feat: set theme, fonts, and frame opacity

--- config.el
+++ config.el
@@ -5,77 +5,69 @@
 ;; Doom's primary configuration file, where 99.9% of your private configuration
 ;; should be written. This file loads after all other Doom modules have loaded.
 ;;
+;; Doom provides the following functions/macros for configuration:
+;;
+;;  - `use-package!' for configuring packages
+;;  - `after!' for running code after a package has loaded
+;;  - `add-load-path' for adding directories to the `load-path', which are
+;;    relative to this file. Emacs searches the `load-path' when you load
+;;    packages with `require' or `use-package'.
+;;  - `load!' for loading external elisp files relative to this one
+;;  - `map!' for binding new keys
+;;
+;; In general, whenever you reconfigure a package that's included by Doom you'll
+;; want to wrap it in an `after!' block, otherwise Doom's defaults may override
+;; your settings. The exceptions to this rule are as follows:
+;;
+;;  - Setting doom variables (these start with 'doom-' or '+').
+;;  - Setting file/directory variables (e.g., `org-directory').
+;;  - Setting variables which explicitly tell you to set them before their
+;;    package is loaded.
+;;
 ;;; Code:
 
+;;;; Setup
+;; ----------------------------------------------------------------------
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Ethan Schmidt"
+      user-mail-address "es.schmidt.cs@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;;; Styling
+;; ----------------------------------------------------------------------
+(setq doom-theme 'doom-spacegrey)
+
+;; Sets the default opacity across both active and inactive frames.
+(add-to-list 'default-frame-alist '(alpha . (95 95)))
+
+;; Sets the fonts used by Doom. There are five (optional) variables for
+;; controlling Doom's fonts:
 ;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;  - `doom-font': The primary font to use.
+;;  - `doom-variable-pitch-font': A non-monospace font (used where applicable).
+;;  - `doom-big-font': Used for `doom-big-font-mode'.
+;;  - `doom-unicode-font': Used for unicode glyphs.
+;;  - `doom-serif-font': Used for the `fixed-pitch-serif' face.
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
+;;
+(setq doom-font (font-spec :family "Source Code Pro" :size 13 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 12))
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
+;;;; Editing
+;; ----------------------------------------------------------------------
+;; Sets the type of line numbering used by `display-line-numbers-mode'. If set
+;; to `nil' line numbers are disabled. For relative numbering, set this to
+;; `relative'. (default t)
 (setq display-line-numbers-type t)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
+;;;; Org Mode
+;; ----------------------------------------------------------------------
+;; Sets the default location where org files are created. It must be set before
+;; org loads.
 (setq org-directory "~/org/")
-
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
 
 ;;; config.el ends here
